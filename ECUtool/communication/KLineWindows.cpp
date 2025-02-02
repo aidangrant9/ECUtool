501450c7904ba09c2f67beefafd33e6fb31f46d1
Aidan Grant <psyag10@exmail.nottingham.ac.uk>
2025-02-02 00:16:01 +0000

    FastInit & FiveBaud implementation

--- ECUtool/communication/KLineWindows.cpp
+++ ECUtool/communication/KLineWindows.cpp
@@ -2,11 +2,16 @@
 
 #include <iostream>
 #include <chrono>
+#include "VecStream.hpp"
 
-KLineWindows::KLineWindows(std::string &portName, size_t baudRate, size_t byteSize, Parity parity, StopBits stopBits, InitMode mode)
-	: SerialConnection(portName, baudRate, byteSize, parity, stopBits), mode{ mode }
-{
-}
+KLineWindows::KLineWindows(std::string &portName, size_t baudRate, size_t byteSize, Parity parity, StopBits stopBits)
+	: SerialConnection(portName, baudRate, byteSize, parity, stopBits)
+{}
+
+KLineWindows::KLineWindows(std::string &portName, size_t baudRate, size_t byteSize, Parity parity, StopBits stopBits, InitMode initMode, AddressingMode addressingMode,
+	uint8_t sourceAddress, uint8_t targetAddress)
+	: SerialConnection(portName, baudRate, byteSize, parity, stopBits), initMode { initMode }, addressingMode { addressingMode }, sourceAddress { sourceAddress }, targetAddress { targetAddress }
+{}
 
 KLineWindows::~KLineWindows()
 {
@@ -49,17 +54,26 @@ bool KLineWindows::initialise()
 		return false;
 	}
 
+	COMMTIMEOUTS readTimeouts = { 20, 0, 0, p4, 0 };
 
-	if (mode == InitMode::None)
+	// Configure port timeouts
+	if (!SetCommTimeouts(hCom, &readTimeouts))
+	{
+		changeConnectionStatus(ConnectionStatus::Error, "Failed to set com timeouts");
+		return;
+	}
+
+
+	if (!initMode.has_value())
 	{
 		return true;
 	}
-	else if (mode == InitMode::FastInit)
+	else if (initMode.value() == InitMode::FastInit)
 	{
 		if (!setPortConfiguration(this->baudRate, this->byteSize, this->parity, this->stopBits))
 		{
 			changeConnectionStatus(ConnectionStatus::Error, "Failed to set port configuration");
-			return;
+			return false;
 		}
 
 		// Wake up pattern
@@ -70,12 +84,210 @@ bool KLineWindows::initialise()
 		std::this_thread::sleep_for(std::chrono::milliseconds(25)); // T_wup - T_inil
 
 		// Send StartCommunicationMessage
-		write({});
+
+		if (!(addressingMode.has_value() && sourceAddress.has_value() && targetAddress.has_value()))
+		{
+			changeConnectionStatus(ConnectionStatus::Error, "StartCommunication addressing information undefined");
+			return false;
+		}
+
+		uint8_t fmt{};
+
+		if (addressingMode.value() == AddressingMode::Functional)
+		{
+			fmt = 0xC1;
+		}
+		else
+		{
+			fmt = 0x81;
+		}
+
+		uint8_t tgt = targetAddress.value();
+		uint8_t src = sourceAddress.value();
+		uint8_t cs{ fmt + tgt + src + 0x81 };
+
+		std::vector<uint8_t> startCommunicationRequest{ fmt, tgt, src, 0x81, cs };
+		
+		DWORD bytesWritten = 0;
+
+		if (!WriteFile(hCom, startCommunicationRequest.data(), startCommunicationRequest.size(), &bytesWritten, 0))
+		{
+			changeConnectionStatus(ConnectionStatus::Error, "Error sending StartCommunicationRequest");
+			return false;
+		}
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(p2));
+
+		std::vector<uint8_t> messageRead{};
+		uint8_t buf[265];
+		DWORD read = 0;
+
+		if (!ReadFile(hCom, buf, 265, &read, 0))
+		{
+			changeConnectionStatus(ConnectionStatus::Error, "Error reading StartCommunicationResponse");
+			return false;
+		}
+
+		if (read)
+		{
+			for (int i = 0; i < read; i++)
+			{
+				messageRead.push_back(buf[i]);
+			}
+		}
+
+		VecStream stream(messageRead);
+
+		try
+		{
+			uint8_t format = stream.next();
+			uint8_t mode = format >> 6;
+			uint8_t len = format & 0b00111111;
+
+			if (mode > 1)
+			{
+				uint8_t r_tgt = stream.next();
+				uint8_t r_src = stream.next();
+
+				// Check addresses are correct
+				if (r_tgt != src || r_src != tgt)
+				{
+					changeConnectionStatus(ConnectionStatus::Error, "Wrong addresses in StartCommunicationResponse");
+					return false;
+				}
+			}
+
+			// Parse additional byte
+			if (len == 0)
+			{
+				len = stream.next();
+			}
+
+			// Check response identifier
+			if (stream.next() != 0xC1)
+			{
+				changeConnectionStatus(ConnectionStatus::Error, "Negative response in StartCommunicationResponse");
+				return false;
+			}
+
+			this->keyByte1 = stream.next();
+			this->keyByte2 = stream.next();
+
+			uint8_t r_cs{};
+			for (int i = 0; i < messageRead.size() - 1; i++)
+			{
+				r_cs += messageRead[i];
+			}
+
+			if (stream.next() != r_cs)
+			{
+				changeConnectionStatus(ConnectionStatus::Error, "Checksum error on StartCommunicationResponse");
+				return false;
+			}
+		}
+		catch (std::out_of_range e)
+		{
+			changeConnectionStatus(ConnectionStatus::Error, "Error parsing StartCommunicationResponse");
+			return false;
+		}
+
+	}
+	else if (initMode.value() == InitMode::FiveBaud)
+	{
+		if (!setPortConfiguration(5, 8, Parity::None, StopBits::OneStopBit))
+		{
+			changeConnectionStatus(ConnectionStatus::Error, "Failed to set port configuration for Five Baud initialisation");
+			return false;
+		}
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(300)); // w5
+
+		if (!(addressingMode.has_value() && sourceAddress.has_value() && targetAddress.has_value()))
+		{
+			changeConnectionStatus(ConnectionStatus::Error, "StartCommunication addressing information undefined");
+			return false;
+		}
+
+		uint8_t address = targetAddress.value();
+
+		DWORD bytesWritten{ 0 };
+
+		if (!WriteFile(hCom, &address, 1, &bytesWritten, 0))
+		{
+			changeConnectionStatus(ConnectionStatus::Error, "Error sending address byte");
+			return false;
+		}
+
+		if (!setPortConfiguration(this->baudRate, this->byteSize, this->parity, this->stopBits))
+		{
+			changeConnectionStatus(ConnectionStatus::Error, "Failed to set port configuration for Five Baud initialisation");
+			return false;
+		}
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(60));
+
+		uint8_t synchByte{};
+		DWORD read = 0;
+
+		if (!ReadFile(hCom, &synchByte, 1, &read, 0))
+		{
+			changeConnectionStatus(ConnectionStatus::Error, "Error reading Synch Byte");
+			return false;
+		}
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(5));
+
+		uint8_t keyBytes[2];
+
+		if (!ReadFile(hCom, keyBytes, 2, &read, 0))
+		{
+			changeConnectionStatus(ConnectionStatus::Error, "Error reading KeyBytes");
+			return false;
+		}
+
+		keyByte1 = keyBytes[0];
+		keyByte2 = keyBytes[1];
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(25));
+
+		uint8_t inversion = ~keyBytes[1];
+
+		if (!WriteFile(hCom, &inversion, 1, &bytesWritten, 0))
+		{
+			changeConnectionStatus(ConnectionStatus::Error, "Error sending KeyByte inversion");
+			return false;
+		}
+
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(25));
+
+		uint8_t addrInversion {};
+
+		if (!ReadFile(hCom, &addrInversion, 1, &read, 0))
+		{
+			changeConnectionStatus(ConnectionStatus::Error, "Error reading address inversion");
+			return false;
+		}
+
+		if (address != ~addrInversion)
+		{
+			changeConnectionStatus(ConnectionStatus::Error, "Address inversion doesn't match address");
+			return false;
+		}
+
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(p3));
 	}
 	else
 	{
 		changeConnectionStatus(ConnectionStatus::Error, "Initialisation mode not implemented");
 		return false;
+	}
+
+	//Check keybytes if they have been set
+	if (keyByte1.has_value() && keyByte2.has_value())
+	{
+		// check parity bit
 	}
 
 	return true;
@@ -160,22 +372,26 @@ void KLineWindows::poll()
 		return;
 	}
 
+	COMMTIMEOUTS readTimeouts = { 20, 0, 0, p4, 0 };
+
+	// Configure port timeouts
+	if (!SetCommTimeouts(hCom, &readTimeouts))
+	{
+		changeConnectionStatus(ConnectionStatus::Error, "Failed to set com timeouts");
+		return;
+	}
+
+	// Set requested port configuration
 	if (!setPortConfiguration(this->baudRate, this->byteSize, this->parity, this->stopBits))
 	{
 		changeConnectionStatus(ConnectionStatus::Error, "Failed to set port configuration");
 		return;
 	}
 
-
 	PurgeComm(hCom, PURGE_RXCLEAR | PURGE_TXCLEAR);	
 
-	COMMTIMEOUTS readTimeouts = { 20, 0, 0, p4, 0 };
-
-	if (!SetCommTimeouts(hCom, &readTimeouts))
-	{
-		changeConnectionStatus(ConnectionStatus::Error, "Failed to set com timeouts");
-		return;
-	}
+	// Set connection status to connected
+	changeConnectionStatus(ConnectionStatus::Connected);
 
 	while (true)
 	{
--- ECUtool/communication/KLineWindows.hpp
+++ ECUtool/communication/KLineWindows.hpp
@@ -18,8 +18,27 @@ protected:
 		None,
 	};
 
-	// ISO14230 Initialisation mode 
-	InitMode mode;
+	enum class AddressingMode
+	{
+		Functional,
+		Physical,
+	};
+
+	// Initialisation options
+	std::optional<InitMode>       initMode       {};
+	std::optional<AddressingMode> addressingMode {};
+	std::optional<uint8_t>        sourceAddress  {};
+	std::optional<uint8_t>        targetAddress  {};
+
+	// Connection parameters
+
+	std::optional<uint8_t> keyByte1 {};
+	std::optional<uint8_t> keyByte2 {};
+
+	size_t p1{20}; // Maxmimum time in between ECU response bytes
+	size_t p2{25}; // Time between messages
+	size_t p3{55}; // Time between ECU response and new request
+	size_t p4{5};  // Inner byte time for requests
 
 	// Thread for async IO
 	std::jthread workThread;
@@ -32,7 +51,9 @@ protected:
 	virtual bool initialise();
 	virtual bool setPortConfiguration(size_t baudRate, size_t byteSize, Parity parity, StopBits stopBits);
 public:
-	KLineWindows(std::string &portName, size_t baudRate, size_t byteSize, Parity parity, StopBits stopBits, InitMode mode);
+	KLineWindows(std::string &portName, size_t baudRate, size_t byteSize, Parity parity, StopBits stopBits);
+	KLineWindows(std::string &portName, size_t baudRate, size_t byteSize, Parity parity, StopBits stopBits, InitMode initMode, AddressingMode addressingMode,
+		uint8_t sourceAddress, uint8_t targetAddress);
 	~KLineWindows() override;
 
 	virtual void connect()    override;
